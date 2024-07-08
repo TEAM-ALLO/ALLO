8f892d5f0cc745a6cbe2566c49c719c909a6f423
suncastle023 <badabada0619@naver.com>
2024-07-08 22:12:05 +0900

    feat: 커뮤니티 기능 간소하게

--- community/models.py
+++ community/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender} to {self.receiver} at {self.timestamp}'
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class EventNotice(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    event_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import community
+from django.contrib.auth.decorators import login_required
+from .views import (
+    community,chat_room,send_message,post_list,post_detail,create_post,
+    event_list,event_detail, create_event
+    )
 
 urlpatterns = [
-    path('community/', community, name='community'),
+    path('chat/<str:username>/', login_required(chat_room), name='chat_room'),
+    path('send_message/', login_required(send_message), name='send_message'),
+    path('post_list/', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('create_post/', login_required(create_post), name='create_post'),
+    path('', event_list, name='event_list'),
+    path('event/<int:pk>/', event_detail, name='event_detail'),
+    path('community/create_event/', login_required(create_event), name='create_event'),
+
 ]
