be4270644840e9517cce22a5aede8e3b4be62628
nakyeong <yunna3425@naver.com>
2024-07-09 23:55:07 +0900

    feat: 헤더 구현

--- allo/settings.py
+++ allo/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
