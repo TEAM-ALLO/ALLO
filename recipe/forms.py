b65e0f018c37dbe068bd0360077c340b8c8484e9
haerangssa <khj100494@naver.com>
2024-07-13 20:25:24 +0900

    feat:recipe_create 필드 추가 기능

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,12 +1,10 @@
-# forms.py
 from django import forms
 from .models import Recipe
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ['recipe_name', 'ingredients', 'instructions', 'cooking_time', 'servings', 'difficulty', 'image']
-        widgets = {
-            'ingredients': forms.Textarea(attrs={'rows': 3}),
-            'instructions': forms.Textarea(attrs={'rows': 5}),
-        }
+        fields = ['recipe_name', 'cooking_time', 'servings', 'difficulty', 'image']
+
+    ingredients = forms.CharField(widget=forms.HiddenInput(), required=False)
+    instructions = forms.CharField(widget=forms.HiddenInput(), required=False)
