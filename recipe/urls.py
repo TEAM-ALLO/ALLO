a091d149b26766f15dd132d4c50c62d6e7b94a8d
nakyeong <yunna3425@naver.com>
2024-07-17 01:15:02 +0900

    fix: app별 detail 페이지 수정

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import recipe_create_view, recipe_list,recipe_detail, recipe_edit_view, recipe_delete_view, bookmarked_recipes, bookmark_recipe, like_recipe, liked_recipes
+from .views import recipe_create_view, recipe_list,recipe_detail_view, recipe_edit_view, recipe_delete_view, bookmarked_recipes, bookmark_recipe, like_recipe, liked_recipes
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,7 +10,7 @@ app_name = 'recipe_user'
 urlpatterns = [
     path('recipe_create/', login_required(recipe_create_view), name='recipe_create'),
     path('recipe_list/', recipe_list, name='recipe_list'),
-    path('recipe/<int:id>/', recipe_detail ,name='recipe_detail'),
+    path('recipe/<int:id>/', recipe_detail_view ,name='recipe_detail'),
     path('recipe/<int:id>/edit/', login_required(recipe_edit_view), name='recipe_edit'),
     path('recipe/<int:id>/delete/', login_required(recipe_delete_view), name='recipe_delete'),
     path('recipe/<int:id>/like/', like_recipe, name='like_recipe'),
