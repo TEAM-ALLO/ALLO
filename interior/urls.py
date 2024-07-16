ad1a7a958e658423d54b102434e1162c04f5247b
suncastle023 <badabada0619@naver.com>
2024-07-16 20:16:04 +0900

    fix:인테리어,커뮤니티 수정

--- interior/urls.py
+++ interior/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+
 app_name = 'interior_user'
+
 urlpatterns = [
     path('', views.interior_list, name='interior_list'),
-    path('<int:pk>/', views.interior_detail, name='interior_detail'),
-    path('new/', views.interior_new, name='interior_new'),
+    path('post/<int:pk>/', views.interior_detail, name='interior_detail'),
+    path('post/new/', views.interior_new, name='interior_new'),
+    path('post/<int:pk>/edit/', views.interior_update, name='interior_update'),
+    path('post/<int:pk>/delete/', views.interior_delete, name='interior_delete'),
+    path('post/<int:pk>/like/', views.like_interior, name='like_interior'),
+    path('post/<int:pk>/bookmark/', views.bookmark_interior, name='bookmark_interior'),
+    path('bookmarked/', views.bookmarked_interiors, name='bookmarked_interiors'),
 ]
