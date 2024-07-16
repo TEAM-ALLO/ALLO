b38714f8ee18b3047e0742e2d1918f4d9b5412ee
nakyeong <yunna3425@naver.com>
2024-07-16 19:17:37 +0900

    feat: recipe_detail과 post_list 프론트

--- allo/settings.py
+++ allo/settings.py
@@ -141,3 +141,8 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 AUTH_USER_MODEL = 'users.User' 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
