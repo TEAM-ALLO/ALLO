7c6bbd6bb50d17f78b8f63927b5e08b20a1b9109
haerangssa <khj100494@naver.com>
2024-07-17 16:44:15 +0900

    feat: 카테고리 선택기능 추가

--- recipe/models.py
+++ recipe/models.py
@@ -3,6 +3,15 @@ from django.conf import settings
 from django.urls import reverse
 
 class Recipe(models.Model):
+    CATEGORY_CHOICES = [
+        ('korean', '한식'),
+        ('chinese', '중식'),
+        ('western', '양식'),
+        ('japanese', '일식'),
+        ('snack', '분식'),
+        ('others', '기타'),
+    ]
+
     recipe_name = models.CharField(max_length=100, verbose_name="레시피 이름")
     ingredients = models.TextField(verbose_name="재료")
     instructions = models.TextField(verbose_name="조리 방법")
@@ -13,6 +22,7 @@ class Recipe(models.Model):
         ('보통', '보통'),
         ('어려움', '어려움')
     ], verbose_name="난이도")
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, verbose_name="카테고리", default='others')
     image = models.ImageField(upload_to='recipes/', blank=True, null=True, verbose_name="레시피 이미지")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="작성일")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일")
@@ -44,3 +54,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
+    
