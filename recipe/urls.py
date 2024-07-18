deba54ba761884a6b04ef76bcde007d27f8069dd
haerangssa <khj100494@naver.com>
2024-07-18 15:37:19 +0900

    feat: recipe_list 검색, 페이지네이션 추가

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import recipe_create_view, recipe_list,recipe_detail_view, recipe_edit_view, recipe_delete_view, bookmarked_recipes, bookmark_recipe, like_recipe, liked_recipes, comments_delete, comments_create
+from .views import recipe_create_view, RecipeListView,recipe_detail_view, recipe_edit_view, recipe_delete_view, bookmarked_recipes, bookmark_recipe, like_recipe, liked_recipes, comments_delete, comments_create
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,7 +9,7 @@ app_name = 'recipe_user'
 
 urlpatterns = [
     path('recipe_create/', login_required(recipe_create_view), name='recipe_create'),
-    path('recipe_list/', recipe_list, name='recipe_list'),
+    path('recipe_list/', RecipeListView.as_view(), name='recipe_list'),
     path('<int:id>/', recipe_detail_view ,name='recipe_detail'),
     path('<int:id>/edit/', login_required(recipe_edit_view), name='recipe_edit'),
     path('<int:id>/delete/', login_required(recipe_delete_view), name='recipe_delete'),
