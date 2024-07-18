1cfc2c061a3ed4fab5a964106298a91ac250ef42
haerangssa <khj100494@naver.com>
2024-07-18 18:44:56 +0900

    feat: 사이드바-recycle 메인 연결 및 검색기능 생성

--- recycle/admin.py
+++ recycle/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recycle
 
+@admin.register(Recycle)
+class RecycleAdmin(admin.ModelAdmin):
+    list_display = ('name','category', 'description')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
 # Register your models here.
