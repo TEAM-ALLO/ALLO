42d4cde9b5e454051ec69782cdad78e4b2d1f072
suncastle023 <badabada0619@naver.com>
2024-07-19 20:17:08 +0900

    conflict 해결

--- recycle/urls.py
+++ recycle/urls.py
@@ -6,4 +6,7 @@ app_name = "recycle_user"
 urlpatterns = [
     path('', views.recycle_main, name='recycle_main'),
     path('<int:pk>', views.recycle_detail, name='recycle_detail'),
+    path('create/', views.RecycleCreate.as_view(), name='recycle_create'),
+    path('<int:pk>/update/', views.RecycleUpdate.as_view(), name='recycle_update'),
+    path('<int:pk>/delete/', views.RecycleDelete.as_view(), name='recycle_confirm_delete'),
 ]
