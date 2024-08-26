12310fd432da4ecc8e9910a2d8e89efaeecf74ed
suncastle023 <badabada0619@naver.com>
2024-08-26 21:59:38 +0900

    fix: css약간+settings debug

--- allo/settings.py
+++ allo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%zl@ooj^!yyy6vyw6xs)=-&qa$1gx1#zjq=$)xso3@qln%!&e7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'sswu-allo.site']
 
